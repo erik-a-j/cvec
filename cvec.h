94d60a3e600f033d41037f5ecc3b53d5e628e374
Erik Johansson <erik.alexius.johansson@gmail.com>
2025-10-25 01:13:10 +0200

    partly documented

--- cvec.h
+++ cvec.h
@@ -25,28 +25,113 @@ enum cvec_error_t {
 };
 typedef size_t cvec_error_t;
 
-typedef void *(*alloc_fn_t)(size_t);
-typedef void *(*realloc_fn_t)(void *,size_t);
-typedef void  (*free_fn_t)(void *);
+typedef void *(*alloc_fn_t)(size_t size);
+typedef void *(*realloc_fn_t)(void *ptr, size_t size);
+typedef void  (*free_fn_t)(void *ptr);
 typedef void *(*memcpy_fn_t)(void *restrict dst, const void *restrict src, size_t n);
 typedef size_t (*grow_fn_t)(size_t old_nmemb, size_t new_nmemb, size_t memb_size);
 
+/**
+ * @brief see struct definition for info
+ * @note see `cvec_hooks_t cvec_hooks_init(void)`
+ */
 typedef struct cvec_hooks_t {
+
+    /** @brief _typedef_ : `void *(*alloc_fn_t)(size_t size)`
+     *  @param (size_t)size size in bytes
+     *  @return ___on success:___ pointer to start of allocated memory, ___on fail:___ NULL
+     */
     alloc_fn_t   alloc;
+
+    /** @brief _typedef_ : `void *(*realloc_fn_t)(void *ptr, size_t size)`
+     *  @param (void*)ptr pointer to start of allocated memory
+     *  @param (size_t)size size in bytes
+     *  @return ___on success:___ pointer to start of reallocated memory, ___on fail:___ NULL
+     */
     realloc_fn_t realloc;
+
+    /** @brief _typedef_ : `void (*free_fn_t)(void *ptr)`
+     *  @param (void*)ptr pointer to start of allocated memory
+     */
     free_fn_t    free;
+
+    /** @brief _typedef_ : `void *(*memcpy_fn_t)(void *restrict dst, const void *restrict src, size_t n)`
+     *  @param (void*restrict)dst pointer to destination
+     *  @param (void*restrict)src pointer to source
+     *  @param (size_t)n size in bytes
+     *  @return ___on success:___ pointer to start of dst, ___on fail:___ NULL
+     */ 
     memcpy_fn_t  memcpy;
+
+    /** @brief _typedef_ : `size_t (*grow_fn_t)(size_t old_nmemb, size_t new_nmemb, size_t memb_size)`
+     *  @param (size_t)old_nmemb previous number of elements
+     *  @param (size_t)new_nmemb new number of elements
+     *  @param (size_t)memb_size size of one element in bytes
+     *  @return ___on success:___ value above 0, ___on fail:___ 0
+     */
     grow_fn_t    grow;
 } cvec_hooks_t;
 
+/**
+ * @brief see struct definition for info
+ */
 typedef struct cvec_t {
-    void   *data;
-    size_t  nmemb_cap;
-    size_t  nmemb;
-    size_t  memb_size;
-    cvec_error_t error;
-    cvec_hooks_t fn;
+    void   *data;       /*pointer to underlying data*/
+    size_t  nmemb_cap;  /*number of members capacity*/
+    size_t  nmemb;      /*number of members currently used*/
+    size_t  memb_size;  /*size of one member in bytes*/
+    cvec_error_t error; /*error flags, see definition of `enum cvec_error_t`*/
+    cvec_hooks_t fn;    /*hooks for fine control over how instance of cvec_t is handled, see `struct cvec_hooks_t`*/
 } cvec_t;
+
+/**
+ * @brief 
+ * `cvec_*` functions requires that __All__ of the __hooks__ must be valid `pointer to function`.  
+ * If none of the `CVEC_CUSTOM*` preprocessor definitions are defined
+ * prior to including `"cvec.h"`, this function will set each member of returned
+ * `cvec_hooks_t struct` to a default valid `pointer to function`.
+ * 
+ * @attention
+ * All `allocator hooks` will be initialized to `NULL` if `CVEC_CUSTOM_ALLOCATORS` is defined
+ * and `<stdlib.h>` is not included prior to including `"cvec.h"`.
+ * @attention
+ * The `memcpy hook` will be initialized to `NULL` if `CVEC_CUSTOM_MEMCPY` is defined
+ * and `<string.h>` is not included prior to including `"cvec.h"`. 
+ * 
+ * @return `struct cvec_hooks_t`
+ * @note see `struct cvec_hooks_t`
+ */
+static inline cvec_hooks_t cvec_hooks_init(void) {
+    cvec_hooks_t h = {
+        #if defined(CVEC_CUSTOM_ALLOCATORS) && !defined(_STDLIB_H)
+          .alloc = NULL, .realloc = NULL, .free = NULL,
+        #else
+          .alloc = malloc, .realloc = realloc, .free = free,
+        #endif
+        #if defined(CVEC_CUSTOM_MEMCPY) && !defined(_STRING_H)
+          .memcpy = NULL,
+        #else
+          .memcpy = memcpy,
+        #endif
+        .grow = cvec_grow
+    };
+    return h;
+}
+/**
+ * @brief initialize `cvec_t` with default values
+ * @param (cvec_t*)vec pointer to `cvec_t` struct
+ * @param (size_t)memb_size size of underlying type in bytes
+ * @param (cvec_hooks_t)hooks `cvec_hooks_t` struct by value
+ * @note see `cvec_hooks_t cvec_hooks_init(void)`
+ */
+static inline void cvec_init(cvec_t *vec, size_t memb_size, cvec_hooks_t hooks) {
+    vec->data = NULL;
+    vec->nmemb_cap = vec->nmemb = 0;
+    vec->memb_size = memb_size;
+    vec->error = ECVEC_NONE;
+    vec->fn = hooks;
+}
+
 
 static inline size_t cvec_capacity(const cvec_t *vec) { return vec->nmemb_cap * vec->memb_size; }
 static inline size_t cvec_size(const cvec_t *vec) { return vec->nmemb * vec->memb_size; }
@@ -67,6 +152,8 @@ static size_t cvec_grow(size_t old_nmemb, size_t new_nmemb, size_t memb_size) {
         return CVEC_GROW_ERROR;
     return n;
 }
+
+/*** + Internal Helpers ***/
 static inline void *cvec_raw_realloc(cvec_t *vec, size_t size) {
     if (!vec->fn.realloc) { vec->error |= ECVEC_MISSING_REALLOC_FN; return NULL; }
     return vec->fn.realloc(vec->data, size);
@@ -83,13 +170,13 @@ static inline void *cvec_raw_memcpy(cvec_t *vec, void *restrict dst, const void 
     if (!vec->fn.memcpy) { vec->error |= ECVEC_MISSING_MEMCPY_FN; return NULL; }
     return vec->fn.memcpy(dst, src, n);
 }
+/*** - Internal Helpers ***/
 
 static inline void cvec_free(cvec_t *vec) {
     cvec_raw_free(vec, vec->data);
     vec->data = NULL;
     vec->nmemb_cap = vec->nmemb = 0;
 }
-
 static inline int cvec_resize(cvec_t *vec, size_t nmemb) {
     if (nmemb == vec->nmemb_cap) return 0;
     if (nmemb == 0) {
@@ -136,30 +223,7 @@ static inline int cvec_shrink(cvec_t *vec, size_t nmemb) {
     return cvec_resize(vec, nmemb);
 }
 
-static inline void cvec_init(cvec_t *vec, size_t memb_size, cvec_hooks_t hooks) {
-    vec->data = NULL;
-    vec->nmemb_cap = vec->nmemb = 0;
-    vec->memb_size = memb_size;
-    vec->error = ECVEC_NONE;
-    vec->fn = hooks;
-}
 
-static inline cvec_hooks_t cvec_hooks_init(void) {
-    cvec_hooks_t h = {
-        #if defined(CVEC_CUSTOM_ALLOCATORS) && !defined(_STDLIB_H)
-          .alloc = NULL, .realloc = NULL, .free = NULL,
-        #else
-          .alloc = malloc, .realloc = realloc, .free = free,
-        #endif
-        #if defined(CVEC_CUSTOM_MEMCPY) && !defined(_STRING_H)
-          .memcpy = NULL,
-        #else
-          .memcpy = memcpy,
-        #endif
-        .grow = cvec_grow
-    };
-    return h;
-}
 
 static inline void *cvec_at(const cvec_t *vec, size_t index) {
     return (index < vec->nmemb_cap)? (char*)vec->data + index * vec->memb_size : NULL;

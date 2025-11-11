5d0e3490864f789e24613832688cba1f26b7c642
Erik Johansson <erik.alexius.johansson@gmail.com>
2025-11-11 16:26:36 +0100

    more hooks

--- src/cvec_hooks.h
+++ src/cvec_hooks.h
@@ -2,7 +2,12 @@
 #define CVEC_HOOKS_H
 #include "cvec_types.h"
 
-size_t cvec_default_grow(size_t old_nmemb, size_t new_nmemb, size_t memb_size);
+size_t default_cvec_grow(size_t old_nmemb, size_t new_nmemb, size_t memb_size);
+int default_cvec_resize(cvec_t *vec, size_t nmemb);
+int default_cvec_push(cvec_t *vec, const void *elem);
+int default_cvec_pushn(cvec_t *vec, const void *elem, size_t count);
+void *default_cvec_insert(cvec_t *vec, const void *elem, size_t index);
+void *default_cvec_erase(cvec_t *vec, size_t first, size_t last);
 
 #ifndef CVEC_CUSTOM_ALLOCATORS
 #include <stdlib.h>
@@ -35,7 +40,12 @@ static inline void cvec_hooks_init(cvec_hooks_t *hooks, int flag) {
 #else
         .memmove = memmove,
 #endif
-        .grow = cvec_default_grow,
+        .grow = default_cvec_grow,
+        .resize = default_cvec_resize,
+        .push = default_cvec_push,
+        .pushn = default_cvec_pushn,
+        .insert = default_cvec_insert,
+        .erase = default_cvec_erase,
     };
 
     if (flag == CVEC_HOOKS_INIT_OVERWRITE) {
@@ -59,7 +69,100 @@ static inline void cvec_hooks_init(cvec_hooks_t *hooks, int flag) {
         if (!hooks->grow) {
             hooks->grow = default_hooks.grow;
         }
+        if (!hooks->resize) {
+            hooks->resize = default_hooks.resize;
+        }
+        if (!hooks->push) {
+            hooks->push = default_hooks.push;
+        }
+        if (!hooks->pushn) {
+            hooks->pushn = default_hooks.pushn;
+        }
+        if (!hooks->insert) {
+            hooks->insert = default_hooks.insert;
+        }
+        if (!hooks->erase) {
+            hooks->erase = default_hooks.erase;
+        }
     }
+}
+
+static inline void *hooks_raw_alloc(cvec_t *vec, size_t size) {
+    if (!vec->hooks.alloc) {
+        vec->error |= ECVEC_MISSING_HOOK_ALLOC;
+        return NULL;
+    }
+    return vec->hooks.alloc(size);
+}
+static inline void *hooks_raw_realloc(cvec_t *vec, size_t size) {
+    if (!vec->hooks.realloc) {
+        vec->error |= ECVEC_MISSING_HOOK_REALLOC;
+        return NULL;
+    }
+    return vec->hooks.realloc(vec->data, size);
+}
+static inline void hooks_raw_free(cvec_t *vec, void *ptr) {
+    if (!vec->hooks.free) {
+        vec->error |= ECVEC_MISSING_HOOK_FREE;
+        return;
+    }
+    vec->hooks.free(ptr);
+}
+static inline void *hooks_raw_memcpy(cvec_t *vec, void *restrict dst, const void *restrict src, size_t n) {
+    if (!vec->hooks.memcpy) {
+        vec->error |= ECVEC_MISSING_HOOK_MEMCPY;
+        return NULL;
+    }
+    return vec->hooks.memcpy(dst, src, n);
+}
+static inline void *hooks_raw_memmove(cvec_t *vec, void *dst, const void *src, size_t n) {
+    if (!vec->hooks.memmove) {
+        vec->error |= ECVEC_MISSING_HOOK_MEMMOVE;
+        return NULL;
+    }
+    return vec->hooks.memmove(dst, src, n);
+}
+static inline size_t hooks_raw_grow(cvec_t *vec, size_t old_nmemb, size_t new_nmemb, size_t memb_size) {
+    if (!vec->hooks.grow) {
+        vec->error |= ECVEC_MISSING_HOOK_GROW;
+        return 0;
+    }
+    return vec->hooks.grow(old_nmemb, new_nmemb, memb_size);
+}
+static inline int hooks_raw_resize(cvec_t *vec, size_t nmemb) {
+    if (!vec->hooks.resize) {
+        vec->error |= ECVEC_MISSING_HOOK_RESIZE;
+        return -1;
+    }
+    return vec->hooks.resize(vec, nmemb);
+}
+static inline int hooks_raw_push(cvec_t *vec, const void *elem) {
+    if (!vec->hooks.push) {
+        vec->error |= ECVEC_MISSING_HOOK_PUSH;
+        return -1;
+    }
+    return vec->hooks.push(vec, elem);
+}
+static inline int hooks_raw_pushn(cvec_t *vec, const void *elem, size_t count) {
+    if (!vec->hooks.pushn) {
+        vec->error |= ECVEC_MISSING_HOOK_PUSHN;
+        return -1;
+    }
+    return vec->hooks.pushn(vec, elem, count);
+}
+static inline void *hooks_raw_insert(cvec_t *vec, const void *elem, size_t index) {
+    if (!vec->hooks.insert) {
+        vec->error |= ECVEC_MISSING_HOOK_INSERT;
+        return NULL;
+    }
+    return vec->hooks.insert(vec, elem, index);
+}
+static inline void *hooks_raw_erase(cvec_t *vec, size_t first, size_t last) {
+    if (!vec->hooks.erase) {
+        vec->error |= ECVEC_MISSING_HOOK_ERASE;
+        return NULL;
+    }
+    return vec->hooks.erase(vec, first, last);
 }
 
 #endif /*CVEC_HOOKS_H*/
--- src/cvec_types.h
+++ src/cvec_types.h
@@ -3,34 +3,34 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#if __STDC_VERSION__ >= 202311L
-#define CVEC_ERROR_TAG cvec_error_tag : uint_least32_t
-#else
-#define CVEC_ERROR_TAG cvec_error_tag
-#endif
-enum CVEC_ERROR_TAG {
-    ECVEC_NONE = 0u,
-    ECVEC_OVERFLOW = (1u << 0),
-    ECVEC_INVALID_INDEX = (1u << 1),
-    ECVEC_MISSING_ALLOC_FN = (1u << 2),
-    ECVEC_MISSING_REALLOC_FN = (1u << 3),
-    ECVEC_MISSING_FREE_FN = (1u << 4),
-    ECVEC_MISSING_GROW_FN = (1u << 5),
-    ECVEC_MISSING_MEMCPY_FN = (1u << 6),
-    ECVEC_MISSING_MEMMOVE_FN = (1u << 7),
-};
-#if __STDC_VERSION__ >= 202311L
-typedef enum cvec_error_tag cvec_error_t;
-#else
+#define ECVEC_NONE                 0u
+#define ECVEC_OVERFLOW             (1u << 0)
+#define ECVEC_INVALID_INDEX        (1u << 1)
+#define ECVEC_MISSING_HOOK_ALLOC   (1u << 2)
+#define ECVEC_MISSING_HOOK_REALLOC (1u << 3)
+#define ECVEC_MISSING_HOOK_FREE    (1u << 4)
+#define ECVEC_MISSING_HOOK_MEMCPY  (1u << 5)
+#define ECVEC_MISSING_HOOK_MEMMOVE (1u << 6)
+#define ECVEC_MISSING_HOOK_GROW    (1u << 7)
+#define ECVEC_MISSING_HOOK_RESIZE  (1u << 8)
+#define ECVEC_MISSING_HOOK_PUSH    (1u << 9)
+#define ECVEC_MISSING_HOOK_PUSHN   (1u << 10)
+#define ECVEC_MISSING_HOOK_INSERT  (1u << 11)
+#define ECVEC_MISSING_HOOK_ERASE   (1u << 12)
 typedef uint_least32_t cvec_error_t;
-#endif
 
+typedef struct cvec_t cvec_t;
 typedef void *(*alloc_fn_t)(size_t size);
 typedef void *(*realloc_fn_t)(void *ptr, size_t size);
 typedef void (*free_fn_t)(void *ptr);
 typedef void *(*memcpy_fn_t)(void *restrict dst, const void *restrict src, size_t n);
 typedef void *(*memmove_fn_t)(void *dst, const void *src, size_t n);
 typedef size_t (*grow_fn_t)(size_t old_nmemb, size_t new_nmemb, size_t memb_size);
+typedef int (*resize_fn_t)(cvec_t *vec, size_t nmemb);
+typedef int (*push_fn_t)(cvec_t *vec, const void *elem);
+typedef int (*pushn_fn_t)(cvec_t *vec, const void *elem, size_t count);
+typedef void *(*insert_fn_t)(cvec_t *vec, const void *elem, size_t index);
+typedef void *(*erase_fn_t)(cvec_t *vec, size_t first, size_t last);
 
 typedef struct cvec_hooks_t {
     alloc_fn_t alloc;
@@ -39,15 +39,20 @@ typedef struct cvec_hooks_t {
     memcpy_fn_t memcpy;
     memmove_fn_t memmove;
     grow_fn_t grow;
+    resize_fn_t resize;
+    push_fn_t push;
+    pushn_fn_t pushn;
+    insert_fn_t insert;
+    erase_fn_t erase;
 } cvec_hooks_t;
 
-typedef struct cvec_t {
+struct cvec_t {
     void *data;
     size_t nmemb_cap;
     size_t nmemb;
     size_t memb_size;
     cvec_error_t error;
     cvec_hooks_t hooks;
-} cvec_t;
+};
 
 #endif /*CVEC_TYPES_H*/
